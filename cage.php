0d3e452af264a73d0bbc6fb3ac0ca91a6fba47dd
Brian Kelley <bkelley620@gmail.com>
2013-10-21 18:14:03 -0400

    Made most of the html files into php files. An apache server  will now be required to show the files correctly

---
+++ cage.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Rensselaer Rocket Society</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    <style>
+        body {
+            font-family: "Helvetica Neue Light";
+        }
+        .b {
+            font-size: 16px;
+        }
+    </style>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+<script src="../../assets/js/html5shiv.js"></script>
+<script src="../../assets/js/respond.min.js"></script>
+<![endif]-->
+</head>
+<body>
+    <?php include "header.html";?>
+
+    <div class = "row" style = "text-align: center;">
+        <div class = "row-fluid b">
+            <p class = "col-md-6">ONE TRUE GOD</p>
+        </div> 
+        <img src="assets/images/OTG.jpg"/>
+        <img src="assets/images/OTG1.gif"/>
+
+    </div>
+</div>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="//code.jquery.com/jquery.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<script>
+    $("#cage").addClass("active");
+</script> 
+</body>
+</html>
---
+++ rockets.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Rensselaer Rocket Society</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    <style>
+        body {
+            font-family: "Helvetica Neue Light";
+        }
+        .b {
+            font-size: 16px;
+        }
+    </style>
+    
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+<script src="../../assets/js/html5shiv.js"></script>
+<script src="../../assets/js/respond.min.js"></script>
+<![endif]-->
+</head>
+<body>
+    <?php include "header.html";?>
+
+    <div class = "row" style = "text-align: center;">
+        <div class = "row-fluid b">
+            <p class = "col-md-6">ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS ROCKETS </p>
+        </div> 
+        <img src="assets/images/teamRocket.jpg"/>
+        <img src='assets/images/Funny.jpg'/>
+    </div>
+</div>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="//code.jquery.com/jquery.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<script>
+    $("#rockets").addClass("active");
+</script> 
+</body>
+</html>
