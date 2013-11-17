3f8f3b08596e9c1fcc7458d8f11bca7770a26fe3
Brian Kelley <bkelley620@gmail.com>
2013-11-17 16:32:57 -0500

    More stuff. Mostly CSS

--- template.php
+++ template.php
@@ -10,36 +10,57 @@ red: ff2600
 	<link rel="stylesheet" type="text/css" href="assets/css/all.css">
 </head>
 <body>
-	<div id="main">
-		<div id="header">
-			<h1>Rensselaer Rocket Society</h1>
-			<h3>Simul Volamus Supremus</h3>
+	<div id="all">
+		<div id="center">
+			<div id="main">
+				<div id="header">
+					<h1>Rensselaer Rocket Society</h1>
+					<h3>Simul Volamus Supremus</h3>
+				</div>
+				<div id="links">
+					<div class="link first">
+						<a href="balls">Home</a>
+					</div>
+					<div class="link">
+						<a href="balls">Projects</a>
+					</div>
+					<div class="link">
+						<a href="balls">Outreach</a>
+					</div>
+					<div class="link">
+						<a href="balls">Documents</a>
+					</div>
+					<div class="link">
+						<a href="balls">Media</a>
+					</div>
+					<div class="link last">
+						<a href="balls">Contact</a>
+					</div>
+				</div>
+				<div id = "content">
+					<p>Our club was founded in the Spring of 2011 to compete in the 2012 NASA University Student Launch Initiative Competition.  The challenge for this event was to develop and build a high powered rocket that would reach as close to one mile as possible and would carry a scientific payload.  Our 2012 Rocket was over 8 feet tall and could carry a payload that included a GPS, an accelerometer, a camera and a set of dynamically adjusted drag flaps to accurately reach the required 5280 ft.  This year the Rensselaer Rocket Society is competing in the  Intercollegiate Rocket Engineering Competition (IREC).  This Competition involves launching a 10 lb  scientific payload to 10,000 ft.</p>
+
+					<p>Meetings are held every Monday and Wednesday on the 4th Floor of the Ricketts Building from 7 pm to 9 pm.  For more information please email rrs@union.rpi.edu</p>
+				</div>
+
+			</div>
+			<div id="footer">
+				<div id="social">
+					<span id="follow">Follow Us On</span>
+					<div id="pics">
+						<a href="https://www.facebook.com/pages/Rensselaer-Rocket-Society/317568988260692">
+							<img id="facebook" src="assets/images/facebook.jpg" alt="Facebook"/>
+						</a>
+					<!-- <a href="">
+						<img src="assets/images/twitter.png" alt="Twitter">
+					</a> -->
+				</div>
+				<div id="logos">
+					<img src="assets/" alt="">
+				</div>
+			</div>
 		</div>
-		<div id="links">
-			<div class="link">
-				<a href="balls">Home</a>
-			</div>
-			<div class="link">
-				<a href="balls">Projects</a>
-			</div>
-			<div class="link">
-				<a href="balls">Outreach</a>
-			</div>
-			<div class="link">
-				<a href="balls">Documents</a>
-			</div>
-			<div class="link">
-				<a href="balls">Media</a>
-			</div>
-			<div class="link">
-				<a href="balls">Contact</a>
-			</div>
-		</div>
-
-
 	</div>
-	<div id="footer">
-
-	</div>
+</div>
 </body>
 </html>
