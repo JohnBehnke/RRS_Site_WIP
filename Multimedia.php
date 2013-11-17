6650e3ad458a51007b8e2b4934f1d04bd47758ab
Brian Kelley <bkelley620@gmail.com>
2013-11-17 15:39:11 -0500

    Did a lot of stuff. Mostly made template.
    Not sure what else was done

---
+++ Multimedia.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <title>Rensselaer Rocket Society</title>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- Bootstrap -->
+     <link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+     <style>
+          body {
+               font-family: "Helvetica Neue Light";
+          }
+          .b {
+               font-size: 16px;
+          }
+     </style>
+
+     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+<script src="../../assets/js/html5shiv.js"></script>
+<script src="../../assets/js/respond.min.js"></script>
+<![endif]-->
+</head>
+<body>
+ <?php include "header.html";?>
+
+ <div class = "row" >
+     <div class = "row-fluid b">
+          <p class = "col-md-6" style = "text-align: justify;">Put pretty pictures here and things!
+          </p>
+     </div> 
+     <div style = "text-align: center">
+          <img src="assets/images/WIP.png"/>
+     </div>
+</div>
+</div>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="//code.jquery.com/jquery.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ template.php
@@ -0,0 +1,45 @@
+<!-- 
+grey: #cecece 
+red: ff2600
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rensselaer Rocket Society</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/all.css">
+</head>
+<body>
+	<div id="main">
+		<div id="header">
+			<h1>Rensselaer Rocket Society</h1>
+			<h3>Simul Volamus Supremus</h3>
+		</div>
+		<div id="links">
+			<div class="link">
+				<a href="balls">Home</a>
+			</div>
+			<div class="link">
+				<a href="balls">Projects</a>
+			</div>
+			<div class="link">
+				<a href="balls">Outreach</a>
+			</div>
+			<div class="link">
+				<a href="balls">Documents</a>
+			</div>
+			<div class="link">
+				<a href="balls">Media</a>
+			</div>
+			<div class="link">
+				<a href="balls">Contact</a>
+			</div>
+		</div>
+
+
+	</div>
+	<div id="footer">
+
+	</div>
+</body>
+</html>
